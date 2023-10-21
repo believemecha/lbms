17f3d39bdf5002929177d9485ce2cf21c4129251
believebhushan <lbsingh732196@gmail.com>
2023-10-21 10:15:48 +0530

    chnage app password gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
   port:                 587,
   domain:               'gmail.com',
   user_name:            'lbsingh732196@gmail.com',
-  password:             'hyzv ztok ptcg hxzc',
+  password:             'gzzw eabf bilj dths',
   authentication:       'plain',
   enable_starttls_auto: true  }
   config.action_mailer.default_url_options = { host: 'lbms-ajua.onrender.com' }
