5b011b97df985cb0229e3fe9d8ce6447497ccc77
Bharat Bhushan <bharat.bhushan@planetspark.in>
2023-11-08 19:51:29 +0530

    remove wrong

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -3,7 +3,6 @@ class CallLog < ApplicationRecord
     
     belongs_to :user
 
-    after_create :process_data
 
     enum call_type:{
         outgoing: 0,
@@ -13,10 +12,5 @@ class CallLog < ApplicationRecord
 
     def self.ransackable_attributes(auth_object = nil)
         ["call_end_time", "call_start_time", "call_type", "created_at", "duration", "id", "name", "phone_number", "updated_at", "user_id"]
-    end
-
-    private
-    def process_data
-        send_log_to_consumer_portal("http://localhost:6000/",self.to_json)
     end
 end
